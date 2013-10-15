a0fae68ba141823c30687f6dd18c32c76b0aa69d
Roman <grudzinski@gmail.com>
2013-10-15 14:10:38 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+
+var _ = require('lodash');
+var connect = require('connect');
+var twilio = require('twilio');
+var rc = require('rc');
+
+var conf = rc('sns-twilio');
+
+var twilioClient = twilio(conf.twilio.sid, conf.twilio.token);
+var app = connect();
+
+app.use(connect.bodyParser());
+app.use(connect.query());
+app.use(connect.json());
+
+app.use('/sns', function(req, res) {
+	res.end();
+	var toPhone = req.query.toPhone;
+	var message = getMessage(conf.messagePattern, req.body);
+	var twilioUrl = getTwilioUrl(conf.twilioUrlPattern, message);
+	twilioClient.makeCall({
+		to: toPhone,
+		from: conf.fromPhone,
+		url: twilioUrl
+	});
+});
+
+app.use('/twilio', function(req, res) {
+	res.setHeader('Content-Type', 'application/xml');
+	res.write('<?xml version="1.0" encoding="UTF-8"?>');
+	res.write('<Response>');
+	res.write('<Say>');
+	res.write('<![CDATA[');
+	res.write(req.query.message);
+	res.write(']]>');
+	res.write('</Say>');
+	res.end('</Response>');
+});
+
+function getMessage(pattern, values) {
+	return pattern.replace(/\{(\w+)\}/g, function(m, token) {
+		var value = values[token];
+		return _.isUndefined(value) ? '' : value;
+	});
+}
+
+function getTwilioUrl(urlPattern, message) {
+	var urlEncodedMessage = encodeURIComponent(message);
+	return urlPattern.replace('{urlEncodedMessage}', urlEncodedMessage);
+}
+
+app.listen(conf.port);
